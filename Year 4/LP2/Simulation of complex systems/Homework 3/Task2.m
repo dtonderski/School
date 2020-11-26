785ffae1f6377559ce55eea9e88b1e12119fc046
dtonderski <dtonderski@gmail.com>
2020-11-26 11:29:20 +0100

    Created a video or two

--- Year 4/LP2/Simulation of complex systems/Homework 3/Task2.m
+++ Year 4/LP2/Simulation of complex systems/Homework 3/Task2.m
@@ -13,14 +13,15 @@ timeSteps = 10000;
 torqueConstant = 100;
 bounceOfWalls = true;
 repeats = 5;
-
+FPS = 100;
 
 [xParticles, yParticles, vParticles, phiParticles] = InitializeParticles(xRange,yRange, vRange,nParticles, radius, repeats);
 [xTrajectories,yTrajectories] = InitializeTrajectories(xParticles,yParticles, nParticles, timeSteps);
 [xStatic, yStatic] = InitializeStaticParticles(xRange, yRange, nStatic, radius, repeats);
 
 f = figure(1);
-F(timeSteps) = getframe(gcf);
+nFrames = timeSteps*dt*FPS;
+F(nFrames) = getframe(gcf);
 tic
 for t = 1:timeSteps
     if mod(t, timeSteps/100) == 0
@@ -31,12 +32,16 @@ for t = 1:timeSteps
     
     xTrajectories(t,:) = xParticles;
     yTrajectories(t,:) = yParticles;
-    f = figure(1);
-    clf
-    PlotTrajectories2(xTrajectories,yTrajectories, nParticles, xStatic, yStatic, radius, t, xRange, yRange, false)    
-    title(sprintf('$v_{mean} = %d, D_T = %d, D_R = %d, T_0 = %d$, %d particles, $t = %.3f$',...
-        mean(vRange), diffusionTrans, diffusionRot, torqueConstant, nParticles, t*dt), 'interpreter', 'latex')
-    F(t) = getframe(gcf);
+    
+    if mod(t, timeSteps/nFrames) == 0
+        f = figure(1);
+        clf
+        PlotTrajectories2(xTrajectories,yTrajectories, nParticles, xStatic, yStatic, radius, t, xRange, yRange, false)    
+        title(sprintf('$v_{mean} = %d, D_T = %d, D_R = %d, T_0 = %d$, %d particles, $t = %.3f$',...
+            mean(vRange), diffusionTrans, diffusionRot, torqueConstant, nParticles, t*dt), 'interpreter', 'latex')
+        F(t*nFrames/timeSteps) = getframe(gcf);
+    end
+    
 end
 toc
 % figure(2)
@@ -44,7 +49,7 @@ toc
 % PlotTrajectories(xTrajectories,yTrajectories, nParticles, xStatic, yStatic, t, xRange, yRange, false)
 
 
-writerObj = VideoWriter('myVideo.avi');
+writerObj = VideoWriter(sprintf('Task2Videos/T0%d.avi', torqueConstant));
 writerObj.FrameRate = round(1/dt);
 open(writerObj);
 for i=1:length(F)
