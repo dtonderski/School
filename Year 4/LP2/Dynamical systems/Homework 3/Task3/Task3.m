f5a09bbea211db28c33e25db4a9a9c0a8043b318
dtonderski <dtonderski@gmail.com>
2020-12-14 19:21:07 +0100

    Update

--- Year 4/LP2/Dynamical systems/Homework 3/Task3/Task3.m
+++ Year 4/LP2/Dynamical systems/Homework 3/Task3/Task3.m
@@ -15,19 +15,21 @@ start = [1,1,1];
 [~,xData] = ode45(@(t,x) f(t,x), [0 t_to_discard], start);
 x0 = xData(end, :)';
 
-dt = 1e-7;
-T = 1e2;
+dt = 1e-6;%1e-7;
+T = 1e3;
 %T = t_to_discard + dt;
 timeVector = t_to_discard:dt:T;
-figureFactor = 1000;
+figureFactor = 10000;
 
 Q = eye(3);
 lambda = zeros(3,1);
 tic
-lambdaData = zeros(length(timeVector)/figureFactor,3);
-for i = 1:length(timeVector)
-    if mod(i, round(length(timeVector)/100)) == 0
-        fprintf('%d %% done.\n', round(100*i/length(timeVector)))
+lambdaData = zeros(ceil(length(timeVector)/figureFactor)-1,3);
+timeData = zeros(ceil(length(timeVector)/figureFactor)-1,1);
+
+for j = 1:length(timeVector)
+    if mod(j, round(length(timeVector)/10000)) == 0
+        fprintf('%.2f %% done.\n', 100*j/length(timeVector))
     end
     x0 = x0 + dt*[sigma*(x0(2) - x0(1)); r*x0(1) - x0(2) - x0(1)*x0(3); x0(1)*x0(2) - b* x0(3)];
     
@@ -38,20 +40,22 @@ for i = 1:length(timeVector)
     M = eye(3) + currentJacobian*dt;
     [Q, R] = qr(M*Q);
     lambda = lambda + log(abs(diag(R)));
-    if mod(i, figureFactor) == 0
-        lambdaData(i/figureFactor, :) = lambda/(timeVector(i)-t_to_discard);
+    if mod(j, figureFactor) == 0
+        lambdaData(j/figureFactor, :) = lambda/(timeVector(j)-t_to_discard);
+        timeData(j/figureFactor) = timeVector(j);
     end
 end
 disp(lambda/(T-t_to_discard))
 toc
 %%
+save('task3c.mat')
 
-for i = 1:3
-    figure(i)
+for j = 1:3
+    figure(j)
     subplot(1,2,1)
 %     plot(timeVector(round(end/2):end), lambdaData(round(end/2):end,i))
-    plot(timeVector, lambdaData(:,i))
+    plot(timeData, lambdaData(:,j))
 
     subplot(1,2,2)
-    semilogx(timeVector, lambdaData(:,i))
+    semilogx(timeData, lambdaData(:,j))
 end
